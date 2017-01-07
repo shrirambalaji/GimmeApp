cd8ed624fb0eb18eac1befbf118afabcf286e744
Naveen <coolnaveen1996@gmail.com>
2017-01-08 00:28:49 +0530

    added requests

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 var express = require('express');
+var flock = require('flockos');
+var myParser = require("body-parser");
 var app = express();
+app.use(myParser.urlencoded({extended : false}));
+app.use(myParser.json());
 
 //post request handles the /events request from flock
 app.post('/events',function(req,res){
-
-  console.log(req);
-res.sendStatus(200);
+  console.log(req.body);
+  res.sendStatus(200);
 });
 
 //this starts the listening on the particular port
 app.listen(80, function () {
   console.log('DailyFeed listening on port 80!');
 });
-
-

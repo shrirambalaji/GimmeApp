680cc2a64cc6063c3320ac9a976a593630551a90
Naveen <coolnaveen1996@gmail.com>
2017-01-09 06:31:59 +0530

    cleaning

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: 80,           // this is the default
+    port: 3000,           // this is the default
     appId: '665e6302-ee68-4ada-b41e-c7950f983b6f',
     appSecret: 'f82d5dc9-7ec7-4221-be56-025b6c7c4f8d',
     dburl: 'mongodb://localhost:27017/DailyFeed'

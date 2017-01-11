e3528a5339e438db2c0c154b5337defeba36a5e8
Naveen <coolnaveen1996@gmail.com>
2017-01-11 22:17:08 +0530

    added weather icons

--- src/app.js
+++ src/app.js
@@ -188,6 +188,10 @@ flock.events.on('client.slashCommand', function(event) {
             }
             //End of news
             break;
+
+
+
+        //Start of weather
         case "weather":
             var uri;
             var current;
@@ -199,6 +203,10 @@ flock.events.on('client.slashCommand', function(event) {
             options = {};
             var locationid;
             var LocalizedName;
+            var State;
+            var Country;
+
+            // GET LOCATION ID
             request.get(locationsuri, options, function(err, res, body) {
                 if (err) {
                     return {
@@ -210,9 +218,9 @@ flock.events.on('client.slashCommand', function(event) {
                     var body = JSON.parse(body);
                     locationid = body[0].Key;
                     LocalizedName = body[0].LocalizedName;
+                    State = body[0].AdministrativeArea.LocalizedName;
+                    Country = body[0].Country.LocalizedName;
                     console.log("LocalizedName" + LocalizedName);
-
-
                     console.log("locationid " + body[0].Key);
 
                     var weatherurl = 'http://dataservice.accuweather.com/currentconditions/v1/' + locationid + '?' + qs.stringify({
@@ -221,6 +229,8 @@ flock.events.on('client.slashCommand', function(event) {
                         details: "true"
                     })
                     options = {};
+
+                    //GET CURRENT WEATHER DETAILS OF ACQUIRED LOCATION ID
                     request.get(weatherurl, options, function(err, res, weatherbody) {
                         if (err) {
                             return {
@@ -229,19 +239,31 @@ flock.events.on('client.slashCommand', function(event) {
                         }
                         var weatherbody = JSON.parse(weatherbody);
                         current = weatherbody[0];
-
-
-
+                        iconid = current.WeatherIcon;
+                        if(iconid<10)
+                        iconurl = "http://developer.accuweather.com/sites/default/files/0" + iconid + "-s.png";
+                        else
+                        iconurl = "http://developer.accuweather.com/sites/default/files/" + iconid + "-s.png";
                         flock.callMethod('chat.sendMessage', config.botToken, {
                                 to: event.chat,
                                 "text": "",
                                 "attachments": [{
-                                    "title": LocalizedName,
-                                    "description": "Temperature: " + current.Temperature.Metric.Value + "\n" +
-                                        "Feels Like: " + current.RealFeelTemperature.Metric.Value + "\n" +
+                                    "title": LocalizedName + ", " + Country,
+                                    "description": "Temperature: " + current.Temperature.Metric.Value  + '\u00B0' +  "C\n" +
+                                        "Feels Like: " + current.RealFeelTemperature.Metric.Value + '\u00B0' +  "C\n" +
                                         "Relative Humidity: " + current.RelativeHumidity + "\n" +
-                                        "Preciptation: " + current.PrecipitationSummary.Past24Hours.Metric.Value + " mm" + "\n" +
-                                        "Sky: " + current.WeatherText,
+                                        "Wind: " + current.Wind.Speed.Metric.Value + " km/h " + current.Wind.Direction.English + "\n" +
+                                        "Precipitation: " + current.PrecipitationSummary.Past24Hours.Metric.Value + " mm" + "\n" +
+                                        "Current Condition: " + current.WeatherText,
+                                      "views": {
+                                        "image": {
+                                            "original": {
+                                                "src": iconurl,
+                                                "width": 150,
+                                                "height": 150
+                                                }
+                                            },
+                                          }
                                 }]
                             },
                             function(error, response) {

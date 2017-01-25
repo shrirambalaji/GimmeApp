9be289947ef0aeed377b0c280f41ead59a7b41f4
Shriram Balaji <Shriram Balaji>
2017-01-26 01:04:24 +0530

    Pushing final code v2.0

---
+++ src/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    port: process.env.PORT || 3000, // this is the default
+    appId: 'f1f3f9b9-e09d-4c9c-aec6-abff6efce823',
+    appSecret: '9164f455-32bf-4331-bbf6-88af3f6ed6fa',
+    dburl: 'mongodb://admin:admin@ds117859.mlab.com:17859/gimmebot',
+    oldbotToken: '4ba2507e-247b-4622-b462-6498aea33508',
+    botToken: 'f88683dd-68d0-47ec-a39f-ad127b8705fe',
+    newsApiKey: '13228478c1034a9db6cca38e772ea590',
+    WeatherApiKey: 'kEe3Pim7DlWqNwQD4XhDCK5ZO9BAQozi',
+    cricApiKey: 'jGYh9DWrbSaIdWGFIICZ0N9IPl93'
+};
---
+++ src/dictionary.js
@@ -0,0 +1,79 @@
+   var express = require('express');
+   var flock = require('flockos');
+   var http = require('http');
+   var config = require('./config');
+   var bodyParser = require("body-parser");
+   var request = require('request');
+   var qs = require('querystring');
+
+   exports.getMeaning = function(word, receiver) {
+       var uri1 = " https://od-api.oxforddictionaries.com/api/v1/entries/en/" + word + "/definitions";
+       options = {
+           headers: {
+               "Accept": "application/json",
+               "app_id": "85a7c394",
+               "app_key": "3097aa0e924a08c216d79e101cc650c4"
+           }
+       };
+       var uri2 = " https://od-api.oxforddictionaries.com/api/v1/entries/en/" + word + "/examples";
+
+       request.get(uri1, options, function(err, res, body) {
+           if (err || res.statusCode == 404) {
+               return {
+                   text: "Could'nt fetch the meaning. Try Again Later."
+               }
+           }
+           console.log("Fetching word definitions");
+           var body = JSON.parse(body);
+           var results = body.results;
+
+
+           var meaning = results[0].lexicalEntries[0].entries[0].senses[0].definitions;
+
+           options = {
+               headers: {
+                   "Accept": "application/json",
+                   "app_id": "85a7c394",
+                   "app_key": "3097aa0e924a08c216d79e101cc650c4"
+               }
+           };
+           request.get(uri2, options, function(err, res, body) {
+               if (err || res.statusCode == 404) {
+                   return {
+                       text: "Could'nt fetch the meaning. Try Again Later."
+                   }
+               }
+               console.log("Fetching word examples");
+               var body = JSON.parse(body);
+               var results = body.results;
+
+
+               var example = results[0].lexicalEntries[0].entries[0].senses[0].examples[0].text;
+               console.log("Sending message");
+                flock.callMethod('chat.sendMessage', config.botToken, {
+                           to: receiver,
+                           "text": "",
+                           "attachments": [{
+                               "title": word,
+                               "color": "#d32d2d",
+                               "description": "Definition : " + meaning + "\n" +
+                                   "Example : " + example + "\n"
+                           }]
+                       },
+                       function(error, response) {
+                           if (error) {
+                               console.log(error);
+                           } else{
+                            console.log("message sent");
+                           }
+                       });
+
+           });
+
+
+
+
+       });
+
+
+   }
